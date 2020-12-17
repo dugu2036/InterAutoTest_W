131aec29f29547595cb54bf82f92fc47109c2891
千叶 <huiselanse@yeah.net>
2020-12-17 16:40:29 +0800

    12.17-下午16:40 testcase文件夹下Test_login.py

--- testcase/Test_login.py
+++ testcase/Test_login.py
@@ -1,31 +1,31 @@
-from config import Conf
-import os
-from utils.YamlUtil import YamlReader
-import pytest
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/23 20:22
+# @File : Test_login.py
+# @Author : Yvon_Fajin
+
 from config.Conf import ConfigYaml
+import pytest
 from utils.RequestsUtil import Request
+
 #1、获取测试用例内容list
 #获取testlogin.yml文件路径
-test_file = os.path.join(Conf.get_data_path(),"testlogin.yml")
-#print(test_file)
-#使用工具类来读取多个文档内容
-data_list = YamlReader(test_file).data_all()
-print(data_list)
+data_list = ConfigYaml().get_testlogin_conf_info()
+# print(data_list)
+
+@pytest.mark.parametrize("login",data_list)  # login 是定义的变量
 #2、参数化执行测试用例
 
-@pytest.mark.parametrize("login",data_list)
 def test_yaml(login):
     #初始化url,data
-    url = ConfigYaml().get_conf_url()+login["url"]
-    print("url %s"%url)
+    url = ConfigYaml().get_conf_url() + login["url"]
+    # print("url %s"%url)
     data = login["data"]
-    print("data %s"%data)
+    # print("data %s"%data)
+
     #post请求
     request = Request()
-    res  = request.post(url,json=data)
-    #打印结果
+    res = request.post(url,json = data)
     print(res)
 
 if __name__ == "__main__":
     pytest.main(["-s","Test_login.py"])
-
