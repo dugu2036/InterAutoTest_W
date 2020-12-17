6478d284cc9906e9959dec9536b1ccde43053dc3
千叶 <huiselanse@yeah.net>
2020-12-17 17:07:20 +0800

    12.17-下午17:10 utils文件夹下剩余4个文件替换

--- utils/MysqlUtil.py
+++ utils/MysqlUtil.py
@@ -1,20 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/19 17:03
+# @File : MysqlUtil.py
+# @Author : Yvon_₯㎕ζ๓
+
 from utils.LogUtil import my_log
 import pymysql
+
 #1、创建封装类
 class Mysql:
-#2、初始化数据，连接数据库，光标对象
-    def __init__(self,host,user,password,database,charset="utf8",port=3306):
+#2、初始化数据、连接数据库、光标对象
+    def __init__(self,host,user,password,database,charset = "utf8",port = 3306):
         self.log = my_log()
         self.conn = pymysql.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            charset=charset,
-            port=port
-            )
-        self.cursor = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
+            host = host,
+            user = user,
+            password = password,
+            database = database,
+            charset = charset,
+            port = port
+           )
+        # 获取执行sql的光标对象
+        # self.cursor = self.conn.cursor()
+        self.cursor = self.conn.cursor(cursor=pymysql.cursors.DictCursor) #光标字典方式查询
 #3、创建查询、执行方法
+
     def fetchone(self,sql):
         """
         单个查询
@@ -36,14 +45,15 @@ class Mysql:
     def exec(self,sql):
         """
         执行
+        :param sql:
         :return:
         """
         try:
             if self.conn and self.cursor:
                 self.cursor.execute(sql)
-                self.conn.commit()
+                self.conn.commit()  # 提交
         except Exception as ex:
-            self.conn.rollback()
+            self.conn.rollback() #数据库回滚
             self.log.error("Mysql 执行失败")
             self.log.error(ex)
             return False
@@ -51,38 +61,64 @@ class Mysql:
 
 #4、关闭对象
     def __del__(self):
+
         #关闭光标对象
         if self.cursor is not None:
             self.cursor.close()
         #关闭连接对象
         if self.conn is not None:
-            self.cursor.close()
+            self.conn.close()
+
+
 
 if __name__ == "__main__":
-    mysql = Mysql("211.103.136.242",
-                  "test",
-                  "test123456","meiduo",
-                  charset="utf8",
-                  port=7090)
-    #res = mysql.fetchall("select username,password from tb_users")
-    res = mysql.exec("update tb_users set first_name='python' where username = 'python'")
+    mysql = Mysql('211.103.136.242','test','test123456',"meiduo",charset = "utf8",port = 7090)
+    #res = mysql.fetchall("select username,password from tb_users") # 查询所有
+    #print(res)
+    Res = mysql.fetchone("select username,password from tb_users") # 查询1条
+    print(Res)
+    res = mysql.exec("update tb_users set first_name='张娟' where username ='18866666666'")
     print(res)
-    #1、创建db_conf.yml, db1,db2
-    #2、编写数据库基本信息
-    #3、重构Conf.py
-    #4、执行
+
+    # 数据放入配置文件
+    #1、创建db_conf.yml,db1,db2
+    #2、编写数据库相关信息
+    #3、重构Conf.py文件
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """
 #1、导入pymysql包
-import pymysql
+
 #2、连接database
 conn = pymysql.connect(
-    host = "211.103.136.242",
-    user = "test",
-    password = "test123456",
+    host = '211.103.136.242',
+    user = 'test',
+    password = 'test123456',
     database = "meiduo",
     charset = "utf8",
-    port =7090
+    port = 7090
 
 )
 #3、获取执行sql的光标对象
@@ -90,8 +126,12 @@ cursor = conn.cursor()
 #4、执行sql
 sql = "select username,password from tb_users"
 cursor.execute(sql)
-res = cursor.fetchone()
+cursor.fetchone() #查询单条数据
+res = cursor.fetchall()  #查询多条数据
 print(res)
 #5、关闭对象
-cursor.close()
-conn.close()"""
+cursor.close() #关闭光标
+conn.close() # 关闭数据库
+"""
+
+
--- utils/RequestsUtil.py
+++ utils/RequestsUtil.py
@@ -1,59 +1,68 @@
-import requests
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2020/10/4 15:21
+@Auth ： Yvon～₯㎕ζ๓
+@File ：RequestsUtil.py
+"""
+import  requests,sys,json
+sys.path.append('../')  # 新加入的
+
+
 from utils.LogUtil import my_log
-#1、创建封装get方法
+
+#创建封装get方法
+
 def requests_get(url,headers):
-#2、发送requests get请求
+    #1、发送get请求
     r = requests.get(url,headers = headers)
-#3、获取结果相应内容
+    #2、获取结果相应内容
     code = r.status_code
     try:
         body = r.json()
     except Exception as e:
         body = r.text
-#4、内容存到字典
+    #3、内容存到字典
     res = dict()
     res["code"] = code
     res["body"] = body
-#5、字典返回
+    #4、字典返回
     return res
 
-#post方法封装
-#1、创建post方法
-def requests_post(url,json=None,headers=None):
-#2、发送post请求
-    r= requests.post(url,json=json,headers=headers)
-#3、获取结果内容
+#创建封装post方法
+def requests_post(url,json = None,headers = None):
+    #1、发送get请求
+    r = requests.post(url,headers = headers,json = json)
+    #2、获取结果相应内容
     code = r.status_code
     try:
         body = r.json()
     except Exception as e:
         body = r.text
-#4、内容存到字典
+    #3、内容存到字典
     res = dict()
     res["code"] = code
     res["body"] = body
-#5、字典返回
+    #4、字典返回
     return res
 
-#重构
-#1、创建类
+"""重构"""
+#1、重建类
 class Request:
 #2、定义公共方法
     def __init__(self):
         self.log = my_log("Requests")
-    def requests_api(self,url,data = None,json=None,headers=None,cookies=None,method="get"):
-
-        if method =="get":
+    #1、增加方法参数，根据参数来验证get/post,请求
+    def requests_api(self,url,data = None,json = None,headers = None,cookies = None,method = 'get'):
+        if method == "get":
             #get请求
             self.log.debug("发送get请求")
-            r = requests.get(url, data = data, json=json, headers=headers,cookies=cookies)
+            r = requests.get(url, data = data,headers = headers,json = json,cookies = cookies)
         elif method == "post":
             #post请求
             self.log.debug("发送post请求")
-            r = requests.post(url,data = data,  json=json, headers=headers,cookies=cookies)
-
-        #2. 重复的内容，复制进来
-        #获取结果内容
+            r = requests.post(url, data = data, headers=headers, json=json,cookies = cookies)
+    #2、重复内容复制进来
+        #获取结果相应内容
         code = r.status_code
         try:
             body = r.json()
@@ -63,20 +72,23 @@ class Request:
         res = dict()
         res["code"] = code
         res["body"] = body
-        #字典返回
+        #4、字典返回
         return res
-
-#3、重构get/post方法
-    #get
-    #1、定义方法
+#3、重构get\post方法
+    #1、get 定义方法
     def get(self,url,**kwargs):
     #2、定义参数
         #url,json,headers,cookies,method
     #3、调用公共方法
-        return self.requests_api(url,method="get",**kwargs)
-
+        return self.requests_api(url,method = 'get',**kwargs)
+        # return json.dumps(self.requests_api(url, method='get', **kwargs),
+        #           sort_keys=True, ensure_ascii=False, indent=4, separators=(', ', ': '))  # Json格式返回
+    #1、post 定义方法
     def post(self,url,**kwargs):
     #2、定义参数
         #url,json,headers,cookies,method
     #3、调用公共方法
-        return self.requests_api(url,method="post",**kwargs)
+        return self.requests_api(url,method = 'post',**kwargs)
+        # return json.dumps(self.requests_api(url,method = 'post',**kwargs),
+        #                   sort_keys=True, ensure_ascii=False, indent=4, separators=(', ', ': ')) # Json格式返回
+
